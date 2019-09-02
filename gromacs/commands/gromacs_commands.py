583587fcaf631d4ae1d65cbc34cabb03df9fcc0b
flongford <flongford@enthought.com>
2019-09-02 17:36:32 +0100

    FEAT: included BaseGromacsProcess class and BaseGromacsCommand subclasses

---
+++ gromacs/commands/gromacs_commands.py
@@ -0,0 +1,96 @@
+from traits.api import (
+    ReadOnly
+)
+
+from gromacs.api import BaseGromacsCommand
+
+
+class Gromacs_genconf(BaseGromacsCommand):
+    """Wrapper around Gromacs genconf command
+    http://manual.gromacs.org/archive/4.6.5/online/genconf.html"""
+
+    #: Name of Gromacs genbox command
+    name = ReadOnly('genconf')
+
+    #: List of accepted flags for Gromacs genbox command
+    flags = ReadOnly(['-f', '-o', '-trj', '-nbox'])
+
+    def __init__(self, name=None, flags=None,
+                 *args, **kwargs):
+        super(Gromacs_genconf, self).__init__(*args, **kwargs)
+
+
+#: NOTE: as of Gromacs 5.0, this tool has been split to gmx solvate
+#: and gmx insert-molecules.
+class Gromacs_genbox(BaseGromacsCommand):
+    """Wrapper around Gromacs genbox command
+    http://manual.gromacs.org/archive/4.6.5/online/genbox.html"""
+
+    #: Name of Gromacs genbox command
+    name = ReadOnly('genbox')
+
+    #: List of accepted flags for Gromacs genbox command
+    flags = ReadOnly(['-cp', '-cs', '-ci', '-maxsol',
+                      '-o', '-box', '-try', '-nmol'])
+
+    def __init__(self, name=None, flags=None,
+                 *args, **kwargs):
+        super(Gromacs_genbox, self).__init__(*args, **kwargs)
+
+
+class Gromacs_grompp(BaseGromacsCommand):
+    """Wrapper around Gromacs grompp command
+    http://manual.gromacs.org/documentation/2018/onlinehelp/gmx-grompp.html"""
+
+    #: Name of Gromacs grompp command
+    name = ReadOnly('grompp')
+
+    #: List of accepted flags for Gromacs grompp command
+    flags = ReadOnly(['-f', '-c', '-r', '-rb', '-n', '-p',
+                      '-t', '-e', '-ref', '-po', '-pp',
+                      '-o', '-idm', '-time', '-maxwarn'])
+
+    def __init__(self, name=None, flags=None,
+                 *args, **kwargs):
+        super(Gromacs_grompp, self).__init__(*args, **kwargs)
+
+
+class Gromacs_genion(BaseGromacsCommand):
+    """Wrapper around Gromacs genion command
+    http://manual.gromacs.org/documentation/2018/onlinehelp/gmx-genion.html"""
+
+    #: Name of Gromacs genion command
+    name = ReadOnly('genion')
+
+    #: List of accepted flags for Gromacs genion command
+    flags = ReadOnly(['-s', '-n', '-p', '-o', '-np', '-pname',
+                      '-pq', '-nn', '-nname', '-nq', '-rmin',
+                      '-seed', '-conc'])
+
+    def __init__(self, name=None, flags=None,
+                 *args, **kwargs):
+        super(Gromacs_genion, self).__init__(*args, **kwargs)
+
+
+class Gromacs_mdrun(BaseGromacsCommand):
+    """Wrapper around Gromacs mdrun command
+    http://manual.gromacs.org/documentation/2018/onlinehelp/gmx-mdrun.html"""
+
+    #: Name of Gromacs genion command
+    name = ReadOnly()
+
+    #: List of accepted flags for Gromacs genion command
+    flags = ReadOnly(['-s', '-g', '-e', '-o', '-x', '-c',
+                      '-cpo'])
+
+    def __init__(self, name=None, flags=None, mpi_run=False,
+                 n_proc=1, *args, **kwargs):
+
+        if mpi_run:
+            name = f'mpirun -np {n_proc} mdrun_mpi'
+        else:
+            name = 'mdrun'
+
+        super(Gromacs_mdrun, self).__init__(
+            name=name, *args, **kwargs
+        )
---
+++ gromacs/commands/tests/test_gromacs_commands.py
@@ -0,0 +1,134 @@
+from unittest import TestCase
+
+from traits.trait_errors import TraitError
+
+from gromacs.api import (
+    Gromacs_genbox, Gromacs_grompp, Gromacs_genion,
+    Gromacs_mdrun, Gromacs_genconf
+)
+
+
+class TestGromacsCommands(TestCase):
+
+    def setUp(self):
+        #: Create Gromacs command objects
+        self.genconf = Gromacs_genconf(dry_run=True)
+        self.genbox = Gromacs_genbox(dry_run=True)
+        self.grompp = Gromacs_grompp(dry_run=True)
+        self.genion = Gromacs_genion(dry_run=True)
+        self.mdrun = Gromacs_mdrun(dry_run=True)
+        self.mdrun_mpi = Gromacs_mdrun(mpi_run=True, dry_run=True)
+
+    def test_readonly(self):
+        with self.assertRaises(TraitError):
+            self.genbox.name = ''
+
+        with self.assertRaises(TraitError):
+            self.genbox.flags = []
+
+    def test_genconf(self):
+
+        input_options = {
+            '-f': 'test_coord.gro',
+            '-o': 'test_output.gro',
+            '-nbox': 30
+        }
+
+        self.genconf.command_options = input_options
+        command = self.genconf.bash_script()
+        self.assertIn('genconf', command)
+        self.assertIn(' -f test_coord.gro', command)
+        self.assertIn(' -o test_output.gro', command)
+        self.assertIn(' -nbox 30', command)
+
+    def test_genbox(self):
+
+        input_options = {
+            '-cp': 'test_coord.gro',
+            '-nmol': 30,
+            '-not_a_flag': 60,
+            '-o': 'test_output.gro',
+            '-try': True}
+
+        self.genbox.command_options = input_options
+        command = self.genbox.bash_script()
+        self.assertIn('genbox', command)
+        self.assertIn(' -cp test_coord.gro', command)
+        self.assertIn(' -o test_output.gro', command)
+        self.assertIn(' -try', command)
+        self.assertIn(' -nmol 30', command)
+        self.assertNotIn(' -not_a_flag 60', command)
+
+    def test_grompp(self):
+
+        input_options = {
+            '-f': 'test_parm.mdp',
+            '-p': 'test_top.top',
+            '-c': 'test_coord.gro',
+            '-o': 'test_top.trp',
+            '-maxwarn': 4,
+            'ci': 'nonsense'}
+        self.grompp.command_options = input_options
+        command = self.grompp.bash_script()
+        self.assertIn('grompp', command)
+        self.assertIn(' -f test_parm.mdp', command)
+        self.assertIn(' -o test_top.trp', command)
+        self.assertIn(' -c test_coord.gro', command)
+        self.assertIn(' -p test_top.top', command)
+        self.assertIn(' -maxwarn 4', command)
+        self.assertNotIn(' ci nonsense', command)
+
+    def test_genion(self):
+
+        input_options = {
+            '-s': 'test_top.trp',
+            '-p': 'test_top.top',
+            '-o': 'test_coord_1.gro',
+            '-pname': 'test_coord_2.gro',
+            '-np': 64,
+            '-pq': 1,
+            '-cp': 'problem'}
+        self.genion.command_options = input_options
+        command = self.genion.bash_script()
+        self.assertIn('genion', command)
+        self.assertIn(' -s test_top.trp ', command)
+        self.assertIn(' -o test_coord_1.gro', command)
+        self.assertIn(' -np 64', command)
+        self.assertIn(' -p test_top.top', command)
+        self.assertIn(' -pname test_coord_2.gro', command)
+        self.assertIn(' -pq 1', command)
+        self.assertNotIn(' -cp problem', command)
+
+        self.genion.user_input = 'W'
+        command = self.genion.bash_script()
+        self.assertIn("echo 'W' | genion", command)
+
+    def test_mdrun(self):
+
+        input_options = {
+            '-s': 'test_top.trp',
+            '-e': 'test_ener.edr',
+            '-o': 'test_traj.trr',
+            '-x': 'test_traj.xtc',
+            '-c': 'test_coord.gro',
+            '-g': 'test_md.log'}
+
+        self.mdrun.command_options = input_options
+        command = self.mdrun.bash_script()
+        self.assertIn('mdrun', command)
+        self.assertIn(' -s test_top.trp ', command)
+        self.assertIn(' -o test_traj.trr', command)
+        self.assertIn(' -g test_md.log', command)
+        self.assertIn(' -e test_ener.edr', command)
+        self.assertIn(' -x test_traj.xtc', command)
+        self.assertIn(' -c test_coord.gro', command)
+
+        self.mdrun_mpi.command_options = input_options
+        command = self.mdrun_mpi.bash_script()
+        self.assertIn('mdrun_mpi', command)
+        self.assertIn(' -s test_top.trp ', command)
+        self.assertIn(' -o test_traj.trr', command)
+        self.assertIn(' -g test_md.log', command)
+        self.assertIn(' -e test_ener.edr', command)
+        self.assertIn(' -x test_traj.xtc', command)
+        self.assertIn(' -c test_coord.gro', command)
