53e7e1cc5674953d9f22babda9bf8020f4276d8a
flongford <flongford@enthought.com>
2019-09-04 10:02:08 +0100

    ENH: added DataSource objects to data_souce.__init__.py

---
+++ force_gromacs/data_sources/__init__.py
@@ -0,0 +1,4 @@
+from .molecule.molecule import Molecule # noqa
+from .molecule.molecule_data_source import MoleculeDataSource # noqa
+from .molecule.molecule_factory import MoleculeFactory # noqa
+from .molecule.molecule_model import MoleculeDataSourceModel # noqa
