ee2d7687ac75f01d2e2dd8d9a042fd8752cd9f34
flongford <flongford@enthought.com>
2019-12-05 10:35:22 +0000

    ENH: included new ISimulationBuilder class

---
+++ force_gromacs/pipeline/i_simulation_builder.py
@@ -0,0 +1,43 @@
+from traits.api import Interface, Unicode, Int, Bool
+
+
+class ISimulationBuilder(Interface):
+    """Class that creates a pipeline for a specific simulation"""
+
+    # --------------------
+    #  Required Attributes
+    # --------------------
+
+    #: Reference name of simulation
+    name = Unicode()
+
+    #: Particle size of simulation
+    size = Int()
+
+    #: Location to create simulation file tree in. (By default,
+    #: the current working directory)
+    directory = Unicode('.')
+
+    #: Location of MARTINI bead parameter file
+    martini_parameters = Unicode()
+
+    #: Whether or not to perform an MPI parallel run
+    mpi_run = Bool(False)
+
+    #: Number of processors required for MPI run
+    n_proc = Int(1)
+
+    #: Whether or not to overwrite existing simulation data
+    ow_data = Bool(False)
+
+    #: Whether or not to perform a dry run
+    dry_run = Bool(True)
+
+    def build_pipeline(self):
+        """Method to be implemented that returns a `GromacsPipeline`
+        object containing all commands required to set up and perform a
+        simulation"""
+
+    def get_results_path(self):
+        """Obtain the results trajectory file path for further
+        post-processing"""
